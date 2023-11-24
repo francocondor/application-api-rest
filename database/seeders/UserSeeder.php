a60b7db7c22c0f6082e418ffc45ad04a3d6fe0da
francobko <franco.condor@urp.edu.pe>
2023-11-23 20:33:37 -0500

    feat: seeders

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    private array $roles = [
+        "super_admin",
+        "natural"
+    ];
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        for ($index = 0; $index <= 10; $index++) {
+            DB::table('users')->insert([
+                'first_name' => 'default' . $index,
+                'last_name' => 'default' . $index,
+                'email' => Str::random(10) . '@default.com',
+                'cellphone' => '12345678',
+                'password' => password_hash('default', PASSWORD_DEFAULT),
+                'state_id' => rand(1,4),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
+        }
+    }
+}
