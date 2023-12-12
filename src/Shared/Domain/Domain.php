58e2145b11c79c2fe51e2038e0d2334841872988
francobko <franco.condor@urp.edu.pe>
2023-12-11 20:57:48 -0500

    Add Domain class to Shared\Domain namespace

---
+++ src/Shared/Domain/Domain.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Src\Shared\Domain;
+
+abstract class Domain
+{
+
+    /**
+     * @param mixed|null $entity
+     * @param string|null $exception
+     */
+    public function __construct(
+        private mixed $entity = null,
+        private readonly ?string $exception = null
+    ) {
+        $this->isException($this->exception);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function entity(): mixed
+    {
+        return $this->entity;
+    }
+
+    protected abstract function isException(?string $exception): never;
+}
