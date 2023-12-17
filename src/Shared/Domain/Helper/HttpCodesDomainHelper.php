f05614aff47479744487673beae5b3dbbfd4646b
francobko <franco.condor@urp.edu.pe>
2023-12-17 17:14:49 -0500

    Add HttpCodesDomainHelper trait

---
+++ src/Shared/Domain/Helper/HttpCodesDomainHelper.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Shared\Domain\Helper;
+
+trait HttpCodesDomainHelper
+{
+
+    public function ok(): int
+    {
+        return 200;
+    }
+
+    public function created(): int
+    {
+        return 201;
+    }
+
+    public function badRequest(): int
+    {
+        return 400;
+    }
+
+    public function unauthorized(): int
+    {
+        return 401;
+    }
+}
