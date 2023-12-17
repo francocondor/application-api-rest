63046b2b5793dbb86937a6f95034baadd3c1e2a9
francobko <franco.condor@urp.edu.pe>
2023-12-17 18:11:58 -0500

    [fix] Add return type hint to jsonResponse method

--- src/Shared/Infrastructure/Controllers/CustomController.php
+++ src/Shared/Infrastructure/Controllers/CustomController.php
@@ -3,6 +3,7 @@
 namespace Src\Shared\Infrastructure\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
 
 abstract class CustomController extends Controller
 {
@@ -14,7 +15,7 @@ abstract class CustomController extends Controller
      * @param array|string|int|bool $response
      * @return JsonResponse
      */
-    protected function jsonResponse(int $status, bool $error, array|string|int|bool $response)
+    protected function jsonResponse(int $status, bool $error, array|string|int|bool $response) :JsonResponse
     {
         return response()->json([
             "status" => $status, 
