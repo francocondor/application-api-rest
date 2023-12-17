abd5834831272b30afb2714f6d19b710a049948c
francobko <franco.condor@urp.edu.pe>
2023-12-17 16:18:44 -0500

    Update exception handling and routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    throw new \Src\Shared\Domain\Exceptions\RequestFailedException('Request failed', 400);
 });
