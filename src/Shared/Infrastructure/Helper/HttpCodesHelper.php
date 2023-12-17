8560d7652abe010243831dba88ff6c9f94098c61
francobko <franco.condor@urp.edu.pe>
2023-12-17 18:12:29 -0500

    [fix] Refactor HttpCodesHelper namespace

--- src/Shared/Infrastructure/Helper/HttpCodesHelper.php
+++ src/Shared/Infrastructure/Helper/HttpCodesHelper.php
@@ -2,30 +2,9 @@
 
 namespace Src\Shared\Infrastructure\Helper;
 
-use App\Shared\Domain\Helper\HttpCodesDomainHelper;
+use Src\Shared\Domain\Helper\HttpCodesDomainHelper;
 
 trait HttpCodesHelper
 {
-
     use HttpCodesDomainHelper;
-
-    // public function getHttpCodeMessage(int $httpCode): string
-    // {
-    //     switch ($httpCode) {
-    //         case 200:
-    //             return 'OK';
-    //         case 201:
-    //             return 'Created';
-    //         case 400:
-    //             return 'Bad Request';
-    //         case 401:
-    //             return 'Unauthorized';
-    //         case 404:
-    //             return 'Not Found';
-    //         case 500:
-    //             return 'Internal Server Error';
-    //         default:
-    //             return 'Unknown';
-    //     }
-    // }
 }
